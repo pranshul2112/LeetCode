6663eadd915256744ba3d57bdfc8304ab4e54931
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-07-07 21:29:47 +0530

    Create 123. Best Time to Buy and Sell Stock III.py

---
+++ 123. Best Time to Buy and Sell Stock III.py
@@ -0,0 +1,13 @@
+#  https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy1, buy2 = 2 ** 32, 2 ** 32
+        profit1, profit2 = 0, 0
+        for price in prices:
+            buy1 = min(buy1, price)
+            profit1 = max(profit1, price - buy1)
+            buy2 = min(buy2, price - profit1)
+            profit2 = max(profit2, price - buy2)
+        return profit2
+        
