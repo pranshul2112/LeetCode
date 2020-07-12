8bcd30bc15167312a43f7ac8c8701d452a0fb2e4
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-07-13 01:20:45 +0530

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,5 +1,26 @@
 #  https://leetcode.com/problems/reverse-integer/
 
+
+# Naive Method 
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        flag = 0
+        res = 0
+        if x < 0:
+            x -= 2 * x
+            flag = 1
+        while x > 0:
+            rem = x % 10
+            res = res * 10 + rem
+            x //= 10
+        if res < - 2 ** 31 or res > 2 ** 31 - 1:
+            return 0
+        if flag == 0:
+            return res
+        return -res
+      
+# Efficent Method 
 class Solution:
     def reverse(self, x: int) -> int:
        
