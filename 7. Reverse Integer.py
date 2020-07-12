40ba0ccc8709aa9bac38612ddb5758f4c7ab948d
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-07-13 01:19:02 +0530

    Create 7. Reverse Integer.py

---
+++ 7. Reverse Integer.py
@@ -0,0 +1,8 @@
+#  https://leetcode.com/problems/reverse-integer/
+
+class Solution:
+    def reverse(self, x: int) -> int:
+       
+        res = int(str(abs(x))[::-1])
+        
+        return (res if x > 0 else -res) if res.bit_length() < 32 else 0 
