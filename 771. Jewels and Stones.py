f11aa2d00ba80c9998a7e5f82fcb4c9a8447825b
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-07-24 20:28:53 +0530

    Create 771. Jewels and Stones.py

---
+++ 771. Jewels and Stones.py
@@ -0,0 +1,9 @@
+#  https://leetcode.com/problems/jewels-and-stones/
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        count = 0
+        for i in range(len(S)):
+            if S[i] in J:
+                count += 1
+        return count 
