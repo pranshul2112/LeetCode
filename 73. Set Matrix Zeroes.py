4ac908e2a0569acbfab60690655a6ab628404fca
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-07-30 12:39:43 +0530

    Update 73. Set Matrix Zeroes.py

--- 73. Set Matrix Zeroes.py
+++ 73. Set Matrix Zeroes.py
@@ -1,32 +1,32 @@
 #  https://leetcode.com/problems/set-matrix-zeroes/
 
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
+class Solution(object):
+    def setZeroes(self, matrix):
         """
-        Do not return anything, modify matrix in-place instead.
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
         """
-        is_col = False 
-        R = len(matrix)
-        C = len(matrix[0])
-        for i in range(R):
+        is_col = False
+        row = len(matrix)
+        col = len(matrix[0])
+        for i in range(row):
+
             if matrix[i][0] == 0:
-                is_col = True 
-                
-            for j in range(1, C):
+                is_col = True
+            for j in range(1, col):
+
                 if matrix[i][j] == 0:
+                    matrix[0][j] = 0
                     matrix[i][0] = 0
-                    matrix[0][j] = 0
-        
-        for i in range(1, R):
-            for j in range(1, C):
-                if matrix[i][0] == 0 or matrix[0][j] == 0:
+
+        for i in range(1, row):
+            for j in range(1, col):
+                if not matrix[i][0] or not matrix[0][j]:
                     matrix[i][j] = 0
-        
-        
         if matrix[0][0] == 0:
-                for j in range(C):
-                    matrix[0][j] = 0
+            for j in range(col):
+                matrix[0][j] = 0
+
         if is_col:
-            for i in range(R):
+            for i in range(row):
                 matrix[i][0] = 0
-        
